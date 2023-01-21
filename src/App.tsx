5988aa179241105e4924278b2ff0dedea8e036a3
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-01-21 16:44:48 +0200

    count working for all button (once state)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
-type Props = {};
-const App = (props: Props) => {
-  return <div>App</div>;
+import { useState } from "react";
+
+type Count = {
+  num: number;
+};
+
+const App = () => {
+  const [count, setCount] = useState<Count>({ num: 0 });
+
+  const onIncrement = (num: number) => {
+    setCount((prevState: Count) => ({
+      ...prevState,
+      num: prevState.num + 1,
+    }));
+  };
+  return (
+    <>
+      <div className="main">
+        <h1 className="title">First test</h1>
+        <button className="btn-1" onClick={() => onIncrement(count.num)}>
+          changeCount({count.num})
+        </button>
+        <button className="btn-2" onClick={() => onIncrement(count.num)}>
+          changeCount({count.num})
+        </button>
+        <button className="btn-3" onClick={() => onIncrement(count.num)}>
+          changeCount({count.num})
+        </button>
+      </div>
+    </>
+  );
 };
 export default App;
